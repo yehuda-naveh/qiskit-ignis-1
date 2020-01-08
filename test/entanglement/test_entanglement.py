a9c4d5bc12df87d40be9ba49a1a9dd75d23c8eda
Yehuda Naveh <naveh@il.ibm.com>
2020-01-08 12:13:41 +0200

    initial test before adjusting to Chris PR

--- test/entanglement/test_entanglement.py
+++ test/entanglement/test_entanglement.py
@@ -10,3 +10,45 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 # pylint: disable=no-name-in-module
+
+
+"""
+Test entanglement functions
+"""
+
+import unittest
+import qiskit
+
+import qiskit.ignis.verification.entanglement
+import qiskit.ignis.verification.entanglement
+
+
+class TestEntanglement(unittest.TestCase):
+    """
+    Test entanglement functions
+    """
+
+    def test_ghz_simple(self):
+        """
+        test that get_ghz_simple correctly creates a ghz circuit
+        """
+
+        qn = 5
+        circ_simple = get_ghz_simple(qn, measure=False)
+        backend = qiskit.Aer.get_backend('qasm_simulator')
+        shots = 1024
+        backend_result = qiskit.execute(
+            circ_simple, backend,
+            shots=shots).result()
+        print(backend_result)
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
